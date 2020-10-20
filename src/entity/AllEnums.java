039345201f68044a85104cfef911cd13f18c12ae
TokJingXian <tokjingxian@gmail.com>
2020-10-21 00:54:22 +0800

    Added AllEnums

---
+++ src/entity/AllEnums.java
@@ -0,0 +1,41 @@
+package entity;
+
+public class AllEnums {
+	enum AccountType{
+		DEFAULT,
+		STUDENT,
+		ADMIN
+	}
+	
+	enum Day {
+		DEFAULT,
+		MONDAY,
+		TUESDAY,
+		WEDNESDAY,
+		THURSDAY,
+		FRIDAY,
+		SATURDAY,
+		SUNDAY
+	}
+	
+	enum Gender {
+		DEFAULT,
+		MALE,
+		FEMALE
+	}
+	
+	enum LessonType {
+		DEFAULT,
+		LECTURE,
+		TUTORIAL,
+		LAB
+	}
+	
+	enum WeekType {
+		DEFAULT,
+		ODD,
+		EVEN,
+		WEEKLY
+	}
+	
+}
